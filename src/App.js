76d0b730301b10de2a2d06a9ded3c19c1d1c6a9a
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2022-01-10 16:24:42 +0100

    Update favicon, text and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import "./App.css";
+import "./App.scss";
 import profile from "./img/profile.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={profile} className="App-logo" alt="logo" />
-        <p>
-          AMINE BOUZAHAR
-          <br />
+        <h1>AMINE BOUZAHAR</h1>
+        <div>
           <code>Coming soon ...</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/amine-bouzahar/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
+        </div>
+        <div className="contact">
+          <ul>
+            <li>
+              <a
+                href="mailto=mohamed.amine.bouzahar@gmail.com"
+                aria-label="Send a mail to mohamed.amine.bouzahar@gmail.com"
+              >
+                <span className="icon">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </span>
+              </a>
+            </li>
+
+            <li>
+              <a
+                className="App-link"
+                href="https://www.linkedin.com/in/amine-bouzahar/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Open linkedIn link"
+              >
+                <span className="icon">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </header>
     </div>
   );
